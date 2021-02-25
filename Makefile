cc326359dee94bef238cae1ae7cf6139dab669f9
Creepercdn <creepercdn@outlook.com>
2021-02-25 23:29:31 +0800

    rebuild code and add makefile for nmake and gnu make

---
+++ Makefile
@@ -0,0 +1,27 @@
+pyrcc = pyrcc5
+pyuic = pyuic5
+lrelease = "lrelease"
+
+rccargs = -compress 12 -threshold 1
+uicargs =
+
+all: res.py untitled.py aboutdiag.py
+
+.PHONY : clean
+clean:
+	rm -rf res.py
+	rm -rf *.qm
+	rm -rf untitled.py
+	rm -rf aboutdiag.py
+
+.PHONY : i18n
+i18n:
+	$(python) compilei18n.py $(lrelease) "./i18n/"
+	mv i18n/untitled.*.qm ./
+
+res.py:
+	$(pyrcc) $(rccargs) res.qrc
+
+untitled.py aboutdiag.py:
+	$(pyuic) $(uicargs) untitled.ui
+	$(pyuic) $(uicargs) aboutdiag.ui
---
+++ compilei18n.py
@@ -0,0 +1,30 @@
+"""
+Usage:
+compilei18n.py <lrelease path> <translate files path> <options to lrelease(DO NOT SET -ts)>
+"""
+
+import subprocess
+import sys
+import os
+import locale
+
+lrelease = 'lrelease' # lrelease path
+workspace = './i18n/'
+options = []
+files = []
+concoding = "utf-8"
+concoding = locale.getpreferredencoding()
+
+if __name__=="__main__":
+    lrelease = sys.argv[1]
+    workspace = sys.argv[2]
+    options = sys.argv[3:]
+
+    files = os.listdir(workspace)
+    for i in range(len(files)):
+        files[i] = os.path.abspath(os.path.join(workspace,files[i]))
+    files = filter(os.path.isfile, files)
+    files = filter(lambda x: os.path.splitext(x)[-1]=='.ts', files)
+
+    for i in files:
+        subprocess.run(f"{os.path.abspath(lrelease)} {' '.join(options)} {os.path.abspath(os.path.join(workspace, i))}")
